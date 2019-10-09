05e5f794b981ad0994ec57282d8f538a99ea3b25
RahulTuli30 <rt3991@g.rit.edu>
2019-10-08 22:57:21 -0700

    Initial Commit

---
+++ functional_tests.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import os
+
+def testDjango():
+    #print(os.getcwd())
+    browser = webdriver.Chrome("./chromedriver")
+    browser.get("http://localhost:8000")
+    assert 'Django' in browser.title, "Django not up and running!"
+
+
+def main():
+    testDjango()
+
+
+if __name__ == "__main__":
+    main()
